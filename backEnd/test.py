db3514d047d5930a9f4340779dc0ee7eff39c8c0
Alipongoy <joshjardenil@mail.fresnostate.edu>
2018-02-12 19:42:05 +0000

    deleted backEnd

--- backEnd/test.py
+++
@@ -1,4 +0,0 @@
-keyArray = [{'trigNumber': 11, 'echoNumber':12, 'description':'front sensor'}, {'trigNumber': 15, 'echoNumber':16, 'description':'back sensor'}]
-for dictionaryObject in keyArray:
-    print(f"This should be the trigNumber for {dictionaryObject['description']}: {dictionaryObject['trigNumber']}");
-    print(f"This should be the echoNumber for {dictionaryObject['description']}: {dictionaryObject['echoNumber']}");
