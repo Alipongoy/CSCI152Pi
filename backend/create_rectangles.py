e77e80c223a102f9a09846f5d36921af2e3dae88
Alan947 <alano79@mail.fresnostate.edu>
2018-04-01 14:44:29 -0700

    Create multiple rectangles (still in progress)

--- backend/create_rectangles.py
+++ backend/create_rectangles.py
@@ -2,6 +2,29 @@ from PIL import Image
 import numpy as np
 import cv2
 import sys
+import time
+
+#To hold the coordidate points (x,y)
+points = [[0 for x in range(2)]for y in range (3)]
+point = []
+#print "Starting from the first parking space, enter 3 points top-left, bottom-left, and top-right"
+#point = raw_input("Points: ")
+#pointsNeeded = ['first', 'second', 'third', 'fourth']
+'''
+for i in range(4):
+	print "Enter x-coordinate of the", pointsNeeded[i], "point"
+	point = raw_input("x-coordinate: ")
+        print "Enter y-coordinate of the", pointsNeeded[i], "point"
+	point = raw_input("y-coordinate: ")
+'''
+'''
+for i in range(len(point)):
+	for j in range(len(point[i])):
+		print("a")
+		print(i, j)
+		print(point[i][j])
+		#time.sleep(1)
+'''
 img1=cv2.imread("/home/pi/Documents/imgA.jpg")
 #cropped the image
 #croppedImg = img1[100:450, 200:700]
@@ -10,12 +33,21 @@ img1=cv2.imread("/home/pi/Documents/imgA.jpg")
 img1 = cv2.resize(img1, (760, 520))
 '''creates a rectangle from given coordinates
 top-left and bottom-right points'''
-#cv2.rectangle(img1,(52,188),(100,220),(255,0,0),3)
+point = [58,193,88,215]
+for i in range(3):
+	#cv2.rectangle(img1,(58,193),(88,215),(255,0,0),2)
+	cv2.rectangle(img1,(58,193),(88,215),(255,0,0),2)
+#convert image to grayscale
+img1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 '''another way of creating rectangle but this way uses
 multiple lines given four coordinate points'''
-pts = np.array([[52,188],[25,215],[88,215],[115,188]],np.int32)
+#[[59,193],[33,215],[87,215],[113,193]] -- 54w 22l
+'''
+pts = np.array([[59,193],[33,215],[87,215],[113,193]],np.int32)
 pts = pts.reshape((-1,1,2))
 cv2.polylines(img1,[pts],True,(255,0,0))
+'''
 cv2.imshow('image',img1)
 cv2.waitKey(0)
+ 
 
