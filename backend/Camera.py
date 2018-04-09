302b237b716df8b3538929705c3683b9a377eb80
Joshua P Jardenil <joshuajardenil@outlook.com>
2018-04-09 11:54:42 -0700

    Finished working on Camera.py

--- backend/Camera.py
+++ backend/Camera.py
@@ -1,13 +1,16 @@
-from PIL import Image
-from picamera import PiCamera
+#from PIL import Image
+#from picamera import PiCamera
 from time import sleep
-import cv2
+#import cv2
 import sys
 import os
+import pdb
 
 class Camera:
     def __init__(self):
-        self.camera = PiCamera()
+        #self.camera = PiCamera()
+        self.__locationToSaveTo = ("/home/pi/Documents/")
+
     
     def _warmUp(self, timeLength):
         sleep(timeLength)
@@ -15,17 +18,28 @@ class Camera:
     def _doesLocationExist(self, filename):
         return os.path.isfile(filename)
     
-    def takePicture(self):
+    def _generateImageLocation(self):
+        counter = 0
+        fullString = self.__locationToSaveTo + "img" + str(counter) + "jpg"
+        while (self._doesLocationExist(fullString)):
+            counter += 1
+            fullString = self.__locationToSaveTo + "img" + str(counter) + "jpg"
+        return fullString
+
+    def setSaveLocation(self, locationToSaveTo):
+        self.__locationToSaveTo = locationToSaveTo
+
+    def takePicture(self, imageName):
+        pdb.set_trace()
         self.camera.start_preview(fullscreen=False, window=(100,20,640,480))
         self._warmUp(3)
 
-        locationToSaveTo = ("/home/pi/Documents/")
-        imageString = "imgA.jpg"
+        imageLocation = self.__locationToSaveTo + imageName
+        self.camera.capture(imageLocation)
 
-        self.camera.capture(locationToSaveTo + imageString)
-
-        print "File is saved to:" + locationToSaveTo + imageString
+        print "File is saved to:" + imageLocation
 
         self.camera.stop_preview()
-        
-        
+
+x = Camera()
+pdb.set_trace()
