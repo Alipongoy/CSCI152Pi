47569c981b0819b1c8a257d9c6e4ef13645dcec1
Joshua P Jardenil <joshuajardenil@outlook.com>
2018-04-23 12:32:01 -0700

    Attepting to work on sending JSON file

--- middleware/push.py
+++ middleware/push.py
@@ -1,5 +1,12 @@
 import urllib2, urllib
+import json
+import pdb
+
+with open('../backend/data.json') as json_data:
+    mydata = json.load(json_data)
+
 mydata = [("lot","lotQ"),("genID","1-1"),("space",3),("isOpen",1)]
+#pdb.set_trace()
 mydata=urllib.urlencode(mydata)
 path = 'http://ab-kc.tk/parking/push.php'
 req=urllib2.Request(path, mydata)
