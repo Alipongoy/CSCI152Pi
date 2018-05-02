2cfdd814e79901826a954b8bd162a0041f3a3db1
Joshua P Jardenil <joshuajardenil@outlook.com>
2018-05-01 21:05:54 -0700

    Can now successfully push to back end via python

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
-from Camera import Camera
+# from Camera import Camera
 from time import sleep
 from ParkingDetection import ParkingDetection
 import cv2
+import urllib2, urllib
+import json
+
+
+def pushToBackEnd(pathToPushTo):
+    with open('../backend/data.json') as json_data:
+        mydata = json.load(json_data)
+
+    req = urllib2.Request(pathToPushTo)
+    req.add_header('Content-Type', 'application/json')
+    response = urllib2.urlopen(req, json.dumps(mydata))
+    print response.read().decode("iso-8859-1")
+    print "Successfully pushedToBackEnd!"
+
 
 def main():
     try:
@@ -16,6 +30,7 @@ def main():
             fullImagePath = camera.takePicture(nameOfImage)
 
             parkingDetection.checkParking(fullImagePath)
+            pushToBackEnd("http://ab-kc.tk/parking/push.php")
             cv2.waitKey(0)
 
     except KeyboardInterrupt:
